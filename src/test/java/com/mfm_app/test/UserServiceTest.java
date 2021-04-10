b3d780fd259a2f3f544800f6e1379c40936b30aa
Polygeekism <irishmangeek@gmail.com>
2021-04-09 20:51:51 -0500

    updated tests.

--- src/test/java/com/mfm_app/test/UserServiceTest.java
+++ src/test/java/com/mfm_app/test/UserServiceTest.java
@@ -9,6 +9,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.beans.HasPropertyWithValue.hasProperty;
 
 import com.mfm_app.entities.User;
 import com.mfm_app.services.UserService;
@@ -22,11 +26,12 @@ class UserServiceTest {
 	@Test
 	@Transactional
 	void get_all_users_test() {
-		List<User> users = new ArrayList<>();
-		users = us.get_all_users();
-		String expected_name = "adam";
+//		List<User> users = new ArrayList<>();
+//		users = us.get_all_users();
+//		String expected_name = "adam";
 		
-		assertTrue(users.contains(expected_name));
+		assertThat(us.get_all_users(), contains(
+				hasProperty("username", is("adam"))));
 	}
 
 }
